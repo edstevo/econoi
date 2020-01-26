5d95dbf385cfdd5c10a20c3d57706bd64f9913d4
Ed Stephenson <edward.stephenson@me.com>
2020-01-26 13:03:40 +0000

    changes

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
-while (i < 10) {
+setTimeout(function () {
 
     console.log("here");
-}
+}, 5000);
