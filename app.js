ea9ae981990bbd26a62a019a24031836207ccd0d
Ed Stephenson <edward.stephenson@me.com>
2020-01-26 12:47:51 +0000

    gpio

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express')
+const Gpio = require('onoff').Gpio;
+
 const app = express()
 const port = 3000
+const i = 0
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+while (i < 10) {
+
+    console.log("here");
+}
