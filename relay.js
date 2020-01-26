2bc0c80d58192e8db12cb1bf49c6e5d976a4f6db
Ed Stephenson <edward.stephenson@me.com>
2020-01-26 18:09:12 +0000

    changes

--- relay.js
+++ relay.js
@@ -1,13 +1,10 @@
 const Gpio  = require('onoff').Gpio;
-const relay = new Gpio(26, 'out');
-
-let value = 1;
+const relay2 = new Gpio(21, 'out');
+const relay1 = new Gpio(26, 'out');
 
 setInterval(function () {
 
-    value   = !value;
-
-    relay.writeSync(relay.readSync() ^ 1);
-    console.log(relay.readSync(value));
+    relay1.writeSync(relay1.readSync() ^ 1);
+    relay2.writeSync(relay2.readSync() ^ 1);
 
 }, 1000);
