59cd236aa68bd3b1bed63114339f381f0a0902a1
Ed Stephenson <edward.stephenson@me.com>
2020-01-28 22:58:17 +0000

    test

--- bridge.js
+++ bridge.js
@@ -3,5 +3,17 @@ const client    = mqtt.connect('mqtt://test.mosquitto.org')
 
 client.on("connect", () => {
 
+    client.subscribe("ines-presence", () => {
+
+        if (!err) {
+            client.publish('ines-presence', 'Hello mqtt')
+        }
+    });
+
     console.log("connected");
 });
+
+client.on('message', function (topic, message) {
+    // message is Buffer
+    console.log(message.toString());
+})
