42df690680d11db7429bc4769b77ac26679ffad9
Ed Stephenson <edward.stephenson@me.com>
2020-01-28 23:01:53 +0000

    message

--- message.js
+++ message.js
@@ -1,4 +1,4 @@
 const mqtt      = require('mqtt');
 const client    = mqtt.connect('mqtt://test.mosquitto.org')
 
-client.publish('ines-presence', 'Hello mqtt')
+client.publish('ines-presence', 'TEST MESSAGE')
